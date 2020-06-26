7517f114517c7fb4f051a2a3d56f43b4b7272657
Chris Yoon <chy.yoon@gmail.com>
2020-06-26 12:19:23 -0700

    fail the isNumber function

--- __tests__/super.test.js
+++ __tests__/super.test.js
@@ -17,4 +17,8 @@ describe('Age', () => {
     expect(user1.planetAge).toContain(16);
     expect(user1.planetAge).toContain(3);
   });
+  test('check if inputAge is a number', () => {
+    const user1 = new Age("not a number");
+    expect(user1.isNumber).toBe(false);
+  });
 });
