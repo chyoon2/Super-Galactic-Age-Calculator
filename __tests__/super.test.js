f2b1066a8298598826606d801e771b839cb9dcec
Chris Yoon <chy.yoon@gmail.com>
2020-06-26 10:05:37 -0700

    pass test for object class age

--- __tests__/super.test.js
+++ __tests__/super.test.js
@@ -1,15 +1,14 @@
 import { Age } from './../src/super.js';
 
-describe('Age', () => {
+describe('Age class', () => {
 let reusableAge;
 
   beforeEach(() => {
     reusableAge = new Age();
   });
 
-  test('should create a Age constructor with inputAge property', () => {
-
-    expect().toEqual();
-
+  test('should create a new age object', () => {
+    const user1 = new Age(30);
+    expect(user1).toMatchObject({inputAge: 30});
   });
 });
--- src/super.js
+++ src/super.js
@@ -1,1 +1,12 @@
+export class Age {
+  constructor(inputAge) {
+    this.inputAge = inputAge;
+  }
 
+  addLine(inputLine1, inputLine2, inputLine3) {
+    this.lineOne.push(inputLine1);
+    this.lineTwo.push(inputLine2);
+    this.lineThree.push(inputLine3);
+  }
+
+}
