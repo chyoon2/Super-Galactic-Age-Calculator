75495703229af58add9eb22b92d3db39c013dad5
Chris Yoon <chy.yoon@gmail.com>
2020-06-26 09:48:50 -0700

    add fail test for Age object

---
+++ __tests__/super.test.js
@@ -0,0 +1,15 @@
+import { Age } from './../src/super.js';
+
+describe('Age', () => {
+let reusableAge;
+
+  beforeEach(() => {
+    reusableAge = new Age();
+  });
+
+  test('should create a Age constructor with inputAge property', () => {
+
+    expect().toEqual();
+
+  });
+});
