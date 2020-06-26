c789d465a2091472d695402afe9c0a3735e2ce31
Chris Yoon <chy.yoon@gmail.com>
2020-06-26 10:38:02 -0700

    fail the planet function test

--- __tests__/super.test.js
+++ __tests__/super.test.js
@@ -7,7 +7,7 @@ let reusableAge;
     reusableAge = new Age();
   });
 
-  test('should create a new age object', () => {
+  test(' should create a new age object', () => {
     const user1 = new Age(30);
     expect(user1).toMatchObject({inputAge: 30});
   });
