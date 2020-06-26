0257b4feba74ebac65b187c838a688cbc6e92884
Chris Yoon <chy.yoon@gmail.com>
2020-06-26 10:45:33 -0700

    correctly fail the planet function test

--- __tests__/super.test.js
+++ __tests__/super.test.js
@@ -7,8 +7,13 @@ let reusableAge;
     reusableAge = new Age();
   });
 
-  test(' should create a new age object', () => {
+  test('should create a new age object', () => {
     const user1 = new Age(30);
     expect(user1).toMatchObject({inputAge: 30});
+  });
+  test('should output a planet adjusted age', () => {
+    const user1 = new Age(30);
+    Age.planet();
+    expect(Age.planetAge).toContain({30});
   });
 });
--- src/super.js
+++ src/super.js
@@ -3,10 +3,10 @@ export class Age {
     this.inputAge = inputAge;
   }
 
-  addLine(inputLine1, inputLine2, inputLine3) {
-    this.lineOne.push(inputLine1);
-    this.lineTwo.push(inputLine2);
-    this.lineThree.push(inputLine3);
+  planet() {
+    // this.planetAge.push(this.inputAge / .24)
+  let venus = this.inputAge / .24;
+  console.log(venus)
   }
 
 }
