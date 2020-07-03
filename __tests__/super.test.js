5df767ccfcf07b9bcbcbc41ebefc444878063137
Chris Yoon <chy.yoon@gmail.com>
2020-07-03 14:22:26 -0700

    fail the test to find the years extended

--- __tests__/super.test.js
+++ __tests__/super.test.js
@@ -66,4 +66,13 @@ describe('Demographic extended', () => {
     expect(user2.planetYearsRemaining).toContain(81);
     expect(user2.planetYearsRemaining).toContain(27);
   });
+  test('should return an array with years extended on each planet', () => {
+    let user2 = new Demographic(90, "Male", "Latinx");
+    user2.calculateLifeExpectancy();
+    user2.calculateYearsExtended();
+    expect(user2.planetYearsExtended).toContain(50);
+    expect(user2.planetYearsExtended).toContain(208);
+    expect(user2.planetYearsExtended).toContain(81);
+    expect(user2.planetYearsExtended).toContain(27);
+  });
 });
